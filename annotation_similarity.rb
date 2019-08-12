05f25895527b4215136d228f548dbde9adb7130c
Rainer Simon <rainer.simon@ait.ac.at>
2019-08-12 14:44:34 +0200

    Computing pair-wise Jaccard similarity

--- annotation_similarity.rb
+++ annotation_similarity.rb
@@ -44,8 +44,7 @@ def build_vector(client, document_id)
   }
 
   '''
-  Response format is as follows:
-
+  ElasticSearch response format is as follows (relevant bits only):
   {
     ...
     aggregations: {
@@ -63,19 +62,64 @@ def build_vector(client, document_id)
   '''
 
   buckets = response['aggregations']['bodies']['uris']['buckets']
-  buckets.map { |b| [ b['key'], b['doc_count'] ] }.to_h
+  buckets.map { |b| [ b['key'], b['doc_count'] ] }
 end
 
+'''
+Each element is a "document descriptor" in the form
+
+{
+  document_id: ...,
+  uris: [
+    [ "http://mygazetteer.org/place/12345": 5 ],
+    ...
+  }
+}
+
+Compare for Jaccard-type similarity (num of intersection vs. num of union)
+'''
+def similarity(a, b)
+  values_a = a[:uris]
+  values_b = b[:uris]
+
+  count_a = values_a.reduce(0) { |sum, tuple| sum + tuple[1] }
+  count_b = values_b.reduce(0) { |sum, tuple| sum + tuple[1] }
+
+  uris_a = values_a.map { |tuple| tuple[0] }
+
+  intersection = values_b.reduce(0) do |sum, tuple| 
+    uri_b = tuple[0]
+    if uris_a.include?(uri_b)
+      sum + tuple[1]
+    else
+     sum
+    end
+  end
+
+  intersection / (count_a + count_b)
+end
+
+'''
+Fetch all document IDs from the DB
+'''
+document_ids = fetch_documents()
+
+puts "Building vectors for #{document_ids.size} documents (may take a while)"
+
 client = Elasticsearch::Client.new
-vectors = fetch_documents()
+vectors = document_ids
   .map { |id| { document_id: id, uris: build_vector(client, id) } }
   .select { |v| !v[:uris].empty? }
 
-puts JSON.pretty_generate(vectors)
+puts "Got #{vectors.size} non-empty vectors documents"
+# puts JSON.pretty_generate(vectors)
+puts "Computing pair-wise similarity"
 
-
-
-
-
-
-
+vectors.each do |outer|
+  # TODO this will compute every pair twice... optimize
+  vectors.each do |inner|
+    if outer != inner 
+      puts "#{outer[:document_id]} - #{inner[:document_id]}: #{similarity(outer, inner)}"
+    end
+  end
+end
