489aa86b122f7a2a4913196b33e56814cb4310b8
Rainer Simon <rainer.simon@ait.ac.at>
2019-08-13 16:18:08 +0200

    DB schema for storing results

---
+++ python/storage/similarities.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+
+class Similarities:
+
+  @staticmethod
+  def store_metadata_similarity():
+    return
+
+  @staticmethod
+  def store_entity_similarity():
+    return
