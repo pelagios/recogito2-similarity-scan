8eb88b6ef5eee43b950e82e27fb2c6e36a4d5fe0
Rainer Simon <rainer.simon@ait.ac.at>
2019-08-14 12:35:09 +0200

    Schema fix

--- python/schema.sql
+++ python/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE similarity (
   doc_id_a TEXT NOT NULL,
   doc_id_b TEXT NOT NULL,
-  title_jaro_winkler DOUBLE PRECISION,
-  entity_jaccard DOUBLE PRECISION,
+  title_jaro_winkler DOUBLE PRECISION DEFAULT 0,
+  entity_jaccard DOUBLE PRECISION DEFAULT 0,
   PRIMARY KEY (doc_id_a, doc_id_b)
 );
